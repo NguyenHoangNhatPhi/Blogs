986280eef1c38551c1ca5f71056c71f5c40a929f
phi.nguyen <phi.nguyen@niteco.se>
2025-10-20 14:16:01 +0700

    Handle post delete

--- a_posts/urls.py
+++ a_posts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home_view, post_create_view
+from .views import home_view, post_create_view,post_delete_view
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('post/create/', post_create_view, name='post-create')
+    path('post/create/', post_create_view, name='post-create'),
+    path('post/delete/<uuid:id>/', post_delete_view, name='post-delete'),
+    
+    
 ]
--- a_posts/views.py
+++ a_posts/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from bs4 import BeautifulSoup
 import requests
 from django.contrib import messages
@@ -7,42 +6,57 @@ from django.contrib import messages
 from .models import Post
 from .forms import PostCreateForm
 
+
 def home_view(request):
     posts = Post.objects.all()
-    return render(request, "a_posts/home.html",{'posts': posts})
+    return render(request, "a_posts/home.html", {"posts": posts})
+
 
 def post_create_view(request):
     form = PostCreateForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostCreateForm(request.POST)
         if form.is_valid():
-            post =form.save(commit=False)
-            
-            website = requests.get(form.data['url'])
-            sourcecode = BeautifulSoup(website.text,'html.parser')
+            post = form.save(commit=False)
 
-            find_image = sourcecode.select('meta[content^="https://live.staticflickr.com/"]')
-            try:   
-                image = find_image[0]['content']
+            website = requests.get(form.data["url"])
+            sourcecode = BeautifulSoup(website.text, "html.parser")
+
+            find_image = sourcecode.select(
+                'meta[content^="https://live.staticflickr.com/"]'
+            )
+            try:
+                image = find_image[0]["content"]
             except:
-                messages.error(request, 'Requested image is not on Flickr!')
-                return redirect('post-create')
-            
+                messages.error(request, "Requested image is not on Flickr!")
+                return redirect("post-create")
+
             post.image = image
-            
-            find_title = sourcecode.select('h1.photo-title')
+
+            find_title = sourcecode.select("h1.photo-title")
             title = find_title[0].text.strip()
             post.title = title
-            
-            find_artist = sourcecode.select('a.owner-name')
-            artist = find_artist[0].text.strip() 
+
+            find_artist = sourcecode.select("a.owner-name")
+            artist = find_artist[0].text.strip()
             post.artist = artist
-            
+
             post.author = request.user
-            
+
             post.save()
             form.save_m2m()
-            return redirect('home')
+            return redirect("home")
         else:
-            return render(request, 'a_posts/post_create.html', {'form': form})
-    return render(request, 'a_posts/post_create.html', {'form': form})
+            return render(request, "a_posts/post_create.html", {"form": form})
+    return render(request, "a_posts/post_create.html", {"form": form})
+
+
+def post_delete_view(request, id):
+    post = get_object_or_404(Post, id=id)
+    
+    if request.method == "POST":
+        post.delete()
+        messages.success(request,"Post deleted")
+        return redirect('home')
+    
+    return render(request, "a_posts/post_delete.html", {"post": post})

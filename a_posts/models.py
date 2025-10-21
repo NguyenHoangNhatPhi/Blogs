ffb09d19a58987d6f3b274466c988cda82ce0722
phi.nguyen <phi.nguyen@niteco.se>
2025-10-21 11:59:27 +0700

    Handle tags for posts

--- a_posts/models.py
+++ a_posts/models.py
@@ -1,3 +1,4 @@
+from time import sleep
 from django.db import models
 import uuid
 
@@ -10,9 +11,17 @@ class Post(models.Model):
     image = models.URLField(max_length=255)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
         return self.title
     
     class Meta:
         ordering = ['-created']
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, unique=True)
+    
+    def __str__(self):
+        return self.name

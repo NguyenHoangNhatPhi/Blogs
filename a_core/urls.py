ebfbc6ca9f638866c464d3dd98437c4d06038a8e
phi.nguyen <phi.nguyen@niteco.se>
2025-10-22 13:41:07 +0700

    Handle static and media

--- a_core/urls.py
+++ a_core/urls.py
@@ -18,14 +18,18 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from a_posts import urls as a_posts_urls
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(a_posts_urls))
 ]
 
+
+
 # Debug Toolbar URLs (only in development)
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     try:
         import debug_toolbar
         urlpatterns = [
